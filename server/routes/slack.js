8d8829071aa535eb518da180718da688889eb453
Hugues ALEXANDRE <hugues@111studio.jp>
2017-02-28 17:55:17 +0900

    change webhook for validation token remove changing username, not working

--- server/routes/slack.js
+++ server/routes/slack.js
@@ -7,10 +7,8 @@ const sendToSlack = (txt, query, reply) => {
   let apiObject = {
     channel: query.channel || '@bolubo',
     response_type: 'in_channel',
-    text: txt,
-    username: `FFBE Wiki - ${query.text}`
+    text: txt
   };
-  //let webHook = process.env.SLACK_WEBHOOK;
   request.post({body:JSON.stringify(apiObject), url:query.response_url}, (err, res, body) => {
     if ( err ) {
       console.warn(err);
@@ -23,22 +21,25 @@ const sendToSlack = (txt, query, reply) => {
 
 module.exports = (request, reply) => {
   try {
-    let [name, ...rest] = request.query.text.split(' ');
-    let ability = rest.join(' ').toLowerCase();
-    let unit = up.getUnitByName(name);
-    if ( unit ) {
-      if ( ['abilities', 'infos', 'magics', 'maxStats', 'sprites', 'stats'].indexOf(ability) !== -1 ) {
-        sendToSlack(unit[ability], request.query, reply);
-      } else {
-        if (ability) {
-          let ab = unit.findAbility(ability) || 'Ability not found';
-          sendToSlack(ab, request.query, reply);
+    let validToken = process.env.SLACK_VALIDTOKEN;
+    if ( validToken === request.query.token ) {
+      let [name, ...rest] = request.query.text.split(' ');
+      let ability = rest.join(' ').toLowerCase();
+      let unit = up.getUnitByName(name);
+      if ( unit ) {
+        if ( ['abilities', 'infos', 'magics', 'maxStats', 'sprites', 'stats'].indexOf(ability) !== -1 ) {
+          sendToSlack(unit[ability], request.query, reply);
         } else {
-          sendToSlack(unit.toString(), request.query, reply);
+          if (ability) {
+            let ab = unit.findAbility(ability) || 'Ability not found';
+            sendToSlack(ab, request.query, reply);
+          } else {
+            sendToSlack(unit.toString(), request.query, reply);
+          }
         }
+      } else {
+        reply('Unit not found');
       }
-    } else {
-      reply('Unit not found');
     }
   } catch (err) {
     reply(err);
